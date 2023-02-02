a7f3a66419d4e2bd37f22398d9d186e4a4911f09
Nonso Egwuagha <egwuaghanonso@gmail.com>
2023-02-02 16:17:05 +0100

    added a simple footer section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,22 +5,26 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-top">
-        <div>
+        <div className="footer-content">
             <h2>Lets Connect!</h2>
             <p>You can find me here</p>
-            <FaEnvelope/>
-            <FaLinkedin/>
-            <FaTwitter/>
-            <FaGithub/>
+          <div>
+          <a href="https://github.com/nonso-ng" target="_blank" rel="noreferrer"> <FaEnvelope/></a>
+            <a href="https://www.linkedin.com/feed/" target="_blank" rel="noreferrer"> <FaLinkedin/> </a>
+            <a href="https://twitter.com/Mazi__nonso" target="_blank" rel="noreferrer"> <FaTwitter/> </a>
+            <a href="https://github.com/nonso-ng" target="_blank" rel="noreferrer"> <FaGithub/> </a>
+          </div>
+            
         </div>
-        <div>
+        <div className="footer-right">
             <h2>Download my Resume here..</h2>
+            <button>Resume</button>
         </div>
       </div>
       <hr/>
       <div className="footer-bottom">
         
-      Copyright © 2022 Egwuagha Nonso
+      Copyright © 2023 Egwuagha Nonso
       </div>
     </div>
   );
